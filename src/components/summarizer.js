1aa37a66258608b8167c8dfb0a6e23ed10093ff7
Rohan Rout <rohan.rout98@gmail.com>
2020-11-13 20:24:36 +0530

    :sparkles:: Add summary length option

--- src/components/summarizer.js
+++ src/components/summarizer.js
@@ -10,11 +10,41 @@ function Summarizer() {
     const [cardData, setCardData] = useState({});
     const [summarizedData, setSummarizedData] = useState({});
     const [displayInt, setDisplayInt] = useState({ blogInt: 0 });
+    const [cardEmpty, setCardEmpty] = useState(0)
+    const [handlerPath, setHandlerPath] = useState(0)
     const axios = require('axios');
     let personUsername = '';
     let forwardSlashIndex = 0;
     let inputURL = '';
+    let summaryLength = 2;
+
+    function shortCall(){
+        summaryLength = 0;
+        setHandlerPath(1)
+        return true
+    }
+
+    function mediumCall(){
+        summaryLength = 1;
+        setHandlerPath(1)
+        return true
+    }
+
+    function longCall(){
+        summaryLength = 2;
+        setHandlerPath(1)
+        return true
+    }
     const submitHandler = (e) => {
+        if(handlerPath === 0)
+            {
+                setCardEmpty(1)
+            }
+        else
+            {
+                setCardEmpty(0)
+            }
+        setHandlerPath(0)
         e.preventDefault();
         if (link.startsWith('https://medium.com/@')) {
             link = link.replace('https://medium.com/', '')
@@ -46,7 +76,7 @@ function Summarizer() {
                             content: (finalData[i].content).replace(/"/g, "").replace(/“/g, "").replace(/”/g, "")
                         });
                         let data = JSON.stringify({ data: cardData.content });
-                        axios.post('https://cors-anywhere.herokuapp.com/http://mlh-summarizer.herokuapp.com/', data, { headers: { "Content-Type": "application/json" } })
+                        axios.post(`https://cors-anywhere.herokuapp.com/http://mlh-summarizer.herokuapp.com/${summaryLength}`, data, { headers: { "Content-Type": "application/json" } })
                             .then(response => {
                                 setSummarizedData({
                                     content: response.data,
@@ -61,7 +91,7 @@ function Summarizer() {
             })
             .catch(err => console.log(err));
     }
-
+    console.log("cardEmpty = "+ cardEmpty)
     return (
         <div className="App">
             <Container className="align-items-left">
@@ -69,26 +99,35 @@ function Summarizer() {
                     <InputGroup className="mb-3 mt-5" size="lg" type="submit">
                         <FormControl id="basic-url" aria-describedby="basic-addon3" type="url" value={link} onChange={(e) => setLink(e.target.value)} placeholder="Enter the Medium blog link here" />
                         <InputGroup.Append>
-                            <Button type="submit" variant="dark">Submit</Button>
+                            <Button type="submit" variant="outline-dark">Submit</Button>
                         </InputGroup.Append>
                     </InputGroup>
+                    {displayInt.blogInt === 1?<div>
+                    <p>Medium article title: <b>{cardData.title}</b></p>
+                    <p>Please select the length of the summary below.</p>
+                    <Button type="submit" variant="outline-dark" onClick={shortCall} className="mx-1 mb-4">Short</Button>
+                    <Button type="submit" variant="outline-dark" onClick={mediumCall} className="mx-1 mb-4">Medium</Button>
+                    <Button type="submit" variant="outline-dark" onClick={longCall} className="mx-1 mb-4">Long</Button>
+                    </div>: null
+                    }
                 </Form>
                 {displayInt.blogInt === 1 ?
-                    <Card className="text-md-left">
-                        <Card.Header as="h5">
-                            {summarizedData.status === 200 ? cardData.title : "Heading"}
-                        </Card.Header>
-                        <Card.Body>
-                            <Card.Text>
-                                {summarizedData.status === 200 ? summarizedData.content : "This is where you'll see your summarized content!"}
-                            </Card.Text>
-                            {summarizedData.status === 200 ? <div>
-                                <Button className="mx-1" variant="outline-dark" href="/">Go back to home</Button>
-                                <Button href={link} variant="dark" target="_blank" className="mx-1">Check original blog</Button>
-                            </div> : null}
-                        </Card.Body>
-                    </Card>
-
+                    <div>
+                        <Card className="text-md-left">
+                            <Card.Header as="h5">
+                                {summarizedData.status === 200 ? cardData.title : "Post Title"}
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    {cardEmpty === 1? "This is where you'll see your summarized content!": summarizedData.status === 200 ? summarizedData.content : "This is where you'll see your summarized content!"}
+                                </Card.Text>
+                                {summarizedData.status === 200 ? <div>
+                                    <Button className="mx-1" variant="outline-dark" href="/">Go back to home</Button>
+                                    <Button href={link} variant="dark" target="_blank" className="mx-1">Check original blog</Button>
+                                </div> : null}
+                            </Card.Body>
+                        </Card>
+                    </div>
                     : null}
                 {cardData === {} ? null : displayInt.blogInt === 0 ? <Blog /> : null}
             </Container>
